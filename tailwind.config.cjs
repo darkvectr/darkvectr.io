05f8dc389066c9d6497548e662aef90e1f9c31c9
r3drun3 <simone.ragonesi@protonmail.com>
2025-07-01 16:09:06 +0200

    feat: first proto version
    Signed-off-by: r3drun3 <simone.ragonesi@protonmail.com>

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        background: "#0d1117",
+        foreground: "#c9d1d9",
+        accent: "#58a6ff"
+      }
+    }
+  },
+  plugins: []
+};
